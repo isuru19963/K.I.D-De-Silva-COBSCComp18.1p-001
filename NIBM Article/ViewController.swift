38a0de6188ac1aec36cb58ed0dfe755d1bc82a1d
Dev19963 <d0153020@student.nibm.lk>
2019-10-28 20:03:18 +0530

    Create Project

---
+++ NIBM Article/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NIBM Article
+//
+//  Created by Isuru Devinda on 10/28/2562 BE.
+//  Copyright © 2562 BE Isuru Devinda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
